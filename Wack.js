11b19888d4ba6931f0256b36e51d9cede060dd18
vcxiz <32043469+vcxiz@users.noreply.github.com>
2017-09-21 18:43:20 -0500

    Update Wack.js

--- Wack.js
+++ Wack.js
@@ -6,13 +6,13 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("a!ping")) {
+  if (message.content === ("a!ping")) {
     message.channel.send({embed: {
       color: 3447003,
       description: "**Pong!** Wack. *Acko? Acko!*"
     }});
   }
-  if (message.content.startsWith("a!help")) {
+  if (message.content === ("a!help")) {
     message.channel.send({embed: {
       color: #ff9933,
       description: "Acko not know help! Acko see under construction!"
