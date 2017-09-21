05287845dd1feff785be0112b04aee1eec675b7d
vcxiz <32043469+vcxiz@users.noreply.github.com>
2017-09-21 18:27:15 -0500

    Update Wack.js

--- Wack.js
+++ Wack.js
@@ -6,9 +6,18 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
+  if (message.content.startsWith("a!ping")) {
+    message.channel.send({embed: {
+      color: 3447003,
+      description: "**Pong!** Wack. *Acko? Acko!*"
+    }});
+  }
+  if (message.content.startsWith("a!help")) {
+    message.channel.send({embed: {
+      color: #ff9933,
+      description: "Acko not know help! Acko see under construction!"
+    }});
   }
 });
-
-client.login("SuperSecretBotTokenHere");
+// Heroku support
+client.login(process.env.BOT_TOKEN);
